63ae4b33a6049d4c11f527ed3844d6fefd49a1eb
Noah Emmet <noah@noahemmet.com>
2020-01-07 02:39:25 -0800

    Add HumanView

---
+++ Client/HumanView.swift
@@ -0,0 +1,21 @@
+import SwiftUI
+
+struct HumanView: View {
+  let human: HumanFragment
+
+  init(human: HumanFragment) {
+    self.human = human
+  }
+  
+  var body: some View {
+    Text(human.name)
+      .font(.body)
+  }
+}
+
+struct HumanView_Previews: PreviewProvider {
+  static let human = HumanFragment(id: "0", name: "Luke Skywalker", homePlanet: "Tattooine")
+  static var previews: some View {
+    HumanView(human: human)
+  }
+}
